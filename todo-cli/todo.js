eedac8aad2eec9b00924199b5d13de0481c58a5b
agarwalaanushree <102991653+agarwalaanushree@users.noreply.github.com>
2023-03-06 20:34:30 +0530

    Update todo.js

--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -31,7 +31,7 @@ const todoList = () => {
     let result = "";
     let n = list.length;
     for (let i = 0; i < n; i++) {
-      if (list[i].dueDate && list[i].dueDate != today) {
+      if (list[i].dueDate != today) {
         if (list[i].completed === true)
           result += "[x] " + list[i].title + " " + list[i].dueDate + "\n";
         else result += "[ ] " + list[i].title + " " + list[i].dueDate + "\n";
